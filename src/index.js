e8d47b2331ed5092af257398b0c53047ee75fa8d
Chris Mihalyi <cmihalyi@gmail.com>
2017-11-01 10:22:14 -0500

    pulling routes into their own file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
-import 'babel-polyfill';
+import 'babel-polyfill'
+import React from 'react'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import configureStore from './app/store/configureStore'
+import {Router, Route, browserHistory} from 'react-router'
+import Routes from './routes'
 
-import React from 'react';
-import {render} from 'react-dom';
-import {Provider} from 'react-redux';
-import App from './app/containers/App';
-import configureStore from './app/store/configureStore';
-import {Router, Route, browserHistory} from 'react-router';
-
-import 'todomvc-app-css/index.css';
+// import 'todomvc-app-css/index.css';
 
 const store = configureStore();
 
 render(
-  <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}/>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
+  <Routes/>, document.getElementById('root')
 );
